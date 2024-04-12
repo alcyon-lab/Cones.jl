12bc5e9bfef598c320af6cc1ed52abc25931954b
İsmail Tapan <ismltpn@gmail.com>
2024-04-12 20:04:15 +0300

    update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,35 @@ using Aqua
 using Test
 
 @testset "Cones.jl" begin
-    # Write your tests here.
+    @testset "CombinationOfCones" begin
+        conepos = Cone([[1, 2], [1, 3]], [4, 3], [false, false], true)
+        coneneg = Cone([[1, 2], [1, 3]], [4, 3], [false, false], false)
+        coneother = Cone([[5, 2], [1, 3]], [4, 3], [true, false], true)
+        combination = CombinationOfCones{Int}()
+        @test size(combination.cones, 1) == 0
+        combination += conepos
+        @test size(combination.cones, 1) == 1
+        @test combination.cones[1][2] == 1
+        combination += conepos
+        @test size(combination.cones, 1) == 1
+        @test combination.cones[1][2] == 2
+        combination += coneneg
+        @test size(combination.cones, 1) == 1
+        @test combination.cones[1][2] == 1
+        combination += coneneg
+        @test size(combination.cones, 1) == 0
+        combination += coneneg
+        @test size(combination.cones, 1) == 1
+        @test combination.cones[1][2] == 1
+        combination += coneneg
+        @test size(combination.cones, 1) == 1
+        @test combination.cones[1][2] == 2
+        combination += coneother
+        @test size(combination.cones, 1) == 2
+        @test combination.cones[1][2] == 2
+    end
 end
 
 @testset "Cones.jl Aqua" begin
-    Aqua.test_all(AlcyonValue)
+    Aqua.test_all(Cones)
 end
