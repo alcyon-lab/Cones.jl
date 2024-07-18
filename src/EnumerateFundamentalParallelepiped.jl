1681b0050cff880c53ccc806bcd680e9ddda2f1f
İsmail Tapan <ismltpn@gmail.com>
2024-07-18 17:38:41 +0300

    v0.1.11, update enumeration for equalities

--- src/EnumerateFundamentalParallelepiped.jl
+++ src/EnumerateFundamentalParallelepiped.jl
@@ -11,8 +11,39 @@ function enumerate_fundamental_parallelepiped(cone::Cone{T}) where {T<:Number}
     dimension = size(vrep, 2) # num of rows
     ambientDimension = size(vrep, 1) # num of cols
     diagonals = Int64[]
+    apex = cone.apex
+    p = zeros(T, (ambientDimension,))
+    if dimension < ambientDimension
+        vrep_lu = lu(vrep)
+        P, L = vrep_lu.P, vrep_lu.L
+        if size(P) != size(L)
+            @warn "dimension mismatch after LU $(size(P)) != $(size(L)), some points might be missing in the result"
+            return Int[]
+        end
+        A_ = L^(-1) * P^(-1)
+        Ap = A_[end-(d-k)+1:end, :]
+        b = Ap * apex
+        m, n = size(Ap)
+        multipliers = [
+            lcm(vcat(denominator.(Ap[i, :]), denominator(b[i])))
+            for i in 1:m
+        ]
+        S_ = Diagonal(multipliers)
+        A, b = S_ * Ap, S_ * b
+
+        SNFRes_ = SmithNormalForm.smith(vrep)
+        S_, Uinv_, Vinv_ = SmithNormalForm.diagm(SNFRes_), SNFRes.Sinv, SNFRes.Tinv
+
+        m, n = size(A)
+        p_ = Uinv_ * b
+        q = vcat([p_[i] // S_[i, i] for i in 1:m], zeros(Int, n - m))
+        p = Vinv_ * q
+        if !all(isinteger.(p))
+            return Int[]
+        end
+    end
     for i in 1:dimension
-        if (i <= size(S, 1) && i <= size(S, 2))
+        if (i <= size(S, 1) && i <= size(S, 2) && S[i, i] != 0)
             push!(diagonals, S[i, i])
         end
     end
@@ -22,9 +53,11 @@ function enumerate_fundamental_parallelepiped(cone::Cone{T}) where {T<:Number}
     for d in diagonals
         push!(sprimeDiagonals, Int64(lastDiagonal // d))
     end
+    for _ in 1:(dimension-length(diagonals))
+        push!(sprimeDiagonals, 1)
+    end
     sprime = diagm(sprimeDiagonals)
-    apex = cone.apex
-    qhat = Uinv * apex
+    qhat = Uinv * (apex - p)
     Wprime = Winv * sprime
     qtrans = [sum([-Wprime[j, i] * qhat[i] for i = 1:dimension]) for j = 1:dimension]
     qfrac = [qtrans[i] - floor(Int, qtrans[i]) for i = 1:dimension]
